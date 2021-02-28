799c9e46912efd330741d2e7b75465d1a4aa45d6
Chloe <yingyingppd@gmail.com>
2021-02-27 22:34:31 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem';
+import CarsList from './components/CarsList';
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        {/* <CarItem name={'Model 3'} tagline={'Order Online For'} image={require('./assets/images/Model3.jpeg')} taglineCTA={'Touchless Delivery'}/> */}
+        <Header />
+        <CarsList />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  
+});
